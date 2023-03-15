de0637ec9e5a3bcf35ed9d8149c28eeec0a4ce70
Patrick Flynn <patrick@chainguard.dev>
2023-03-15 08:00:31 -0400

    add dataplaneapi and test (not working)
    Signed-off-by: Patrick Flynn <patrick@chainguard.dev>

--- images/haproxy/tests/03-dataplaneapi-enabled.sh
+++ images/haproxy/tests/03-dataplaneapi-enabled.sh
@@ -5,4 +5,6 @@ set -o errexit -o nounset -o errtrace -o pipefail -x
 IMAGE_DIR="$(basename "$(cd "$(dirname ${BASH_SOURCE[0]})/.." && pwd )")"
 IMAGE_NAME=${IMAGE_NAME:-"cgr.dev/chainguard/${IMAGE_DIR}:latest"}
 
-docker run -it --rm -d --name my-running-haproxy --expose 5555 -v "$(pwd)/configs/dataplaneapi-enabled:/usr/local/etc/haproxy:rw" "${IMAGE_NAME}" -c -f /usr/local/etc/haproxy/haproxy.cfg
+docker run -it --rm --name test-haproxy-config --expose 5555 -v "$(pwd)/configs/dataplaneapi-enabled:/etc/haproxy:rw" "${IMAGE_NAME}" -c -f /etc/haproxy/haproxy.cfg
+docker run -d --name my-running-haproxy --expose 5555 -p 8080:5555 -v "$(pwd)/configs/dataplaneapi-enabled:/etc/haproxy:rw" "${IMAGE_NAME}" -f /etc/haproxy/haproxy.cfg
+curl -u admin:test123 -H "Content-Type: application/json" "http://127.0.0.1:8080/v2/"
