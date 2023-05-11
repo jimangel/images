125f403c508a62475c09e41a46371f2ca900ecad
Josh Dolitsky <josh@dolit.ski>
2023-05-11 17:55:10 -0500

    testing
    Signed-off-by: Josh Dolitsky <josh@dolit.ski>

---
+++ main.tf
@@ -0,0 +1,44 @@
+/*
+Copyright 2023 Chainguard, Inc.
+SPDX-License-Identifier: Apache-2.0
+*/
+
+terraform {
+  required_providers {
+    apko = {
+      source = "chainguard-dev/apko"
+    }
+    cosign = {
+      source = "chainguard-dev/cosign"
+    }
+    oci = {
+      source = "chainguard-dev/oci"
+    }
+  }
+}
+
+variable "apko_config_path" {
+  description = "The path to the apko image config file."
+}
+
+variable "target_repository" {
+  description = "The docker repo into which the image and attestations should be published."
+}
+
+provider "apko" {
+  extra_repositories = ["https://packages.wolfi.dev/os"]
+  extra_keyring      = ["https://packages.wolfi.dev/os/wolfi-signing.rsa.pub"]
+  default_archs      = ["x86_64", "aarch64"]
+  extra_packages     = []
+}
+
+module "image" {
+  source  = "chainguard-dev/apko/publisher"
+
+  target_repository = var.target_repository
+  config = file(var.apko_config_path)
+}
+
+output "image_ref" {
+  value = module.image.image_ref
+}
