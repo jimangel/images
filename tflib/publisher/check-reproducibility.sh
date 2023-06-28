b481d129266d1a86189411aaa50dae19f74a7aa5
jdolitsky <jdolitsky@users.noreply.github.com>
2023-06-28 01:29:57 +0000

    Update images digests
    Signed-off-by: GitHub <noreply@github.com>

--- tflib/publisher/check-reproducibility.sh
+++ tflib/publisher/check-reproducibility.sh
@@ -31,7 +31,7 @@ REBUILT_IMAGE_NAME=$(docker run --rm \
    --link "${container_name}" \
    -v "${TMP}:/tmp/latest.apko.json" \
    -v ${PWD}:${PWD}:ro -w ${PWD} \
-   ghcr.io/wolfi-dev/apko:latest@sha256:4f747c533aa5b2bad01c64ec12e73a9c933510c2918d3c40a0e85b113e014ac3 \
+   ghcr.io/wolfi-dev/apko:latest@sha256:5e803a1a54fe8547937894701af79d97c61970c9d86a55526dea8270a140b913 \
    publish /tmp/latest.apko.json ${container_name}:5000/reproduction
 )
 
